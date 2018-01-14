8217e0128827050752fb99b91120f7b60aeaddbe
llfly <lflylfy@gmail.com>
2018-01-14 16:11:25 +0800

    springboot project init

---
+++ src/main/java/com/llfly/test/Main.java
@@ -0,0 +1,14 @@
+package com.llfly.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fangyou on 2018/1/14.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String [] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/com/llfly/test/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.llfly.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fangyou on 2018/1/14.
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "hello,world";
+    }
+}
